b18d3d7a3b15fcdd5045ff66d63a47b614bd6c4f
Jacob Fletcher <jacobsfletch@gmail.com>
2022-08-20 08:58:36 -0400

    feat: adds redirect middleware

---
+++ src/pages/docs/slider/examples/_middleware.ts
@@ -0,0 +1,9 @@
+import { NextResponse, NextRequest } from 'next/server'
+
+export async function middleware(req: NextRequest) {
+  const { pathname } = req.nextUrl
+  if (pathname === '/docs/slider/examples') {
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/docs/slider/examples/simple-slider`)
+  }
+  return NextResponse.next()
+}
