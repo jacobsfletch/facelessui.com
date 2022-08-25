6594da263ac01e316dd09fe0fed06bba71331a83
Jacob Fletcher <jacobsfletch@gmail.com>
2022-08-25 07:51:08 -0400

    chore: migrates middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,8 +2,12 @@ import { NextResponse, NextRequest } from 'next/server'
 
 export async function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl
+  console.log(pathname);
   if (pathname === '/docs/slider/examples') {
     return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/docs/slider/examples/simple`)
   }
-  return NextResponse.next()
+
+  if (pathname === '/docs/modal/examples') {
+    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/docs/slider/modal/lightbox`)
+  }
 }
--- src/pages/docs/modal/examples/_middleware.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse, NextRequest } from 'next/server'
-
-export async function middleware(req: NextRequest) {
-  const { pathname } = req.nextUrl
-  if (pathname === '/docs/modal/examples') {
-    return NextResponse.redirect(`${process.env.NEXT_PUBLIC_APP_URL}/docs/modal/examples/lightbox`)
-  }
-  return NextResponse.next()
-}
