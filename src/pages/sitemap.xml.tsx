9fe3f055a47f9d3f4adb30c00e9f09e0ae0798ad
Jacob Fletcher <jacobsfletch@gmail.com>
2022-09-05 11:48:43 -0400

    temp: static sitemap

--- src/pages/sitemap.xml.tsx
+++ src/pages/sitemap.xml.tsx
@@ -1,49 +1,103 @@
 import { GetServerSideProps } from "next";
-import { promises as fs } from "fs";
+// import { promises as fs } from "fs";
 
 const Sitemap = () => { };
 
-const blacklistedPages = [
-  '/sitemap.xml',
-  '/.DS_Store',
-  '/404',
-  '/500',
-  '/_error',
-  '/_app',
-  '/_document',
-  '/styleguide',
+// TODO: generate sitemap dynamically from pages, see note below
+const pages = [
+  "/docs/getting-started",
+  "/docs/modal",
+  "/docs/modal/setup",
+  "/docs/modal/routing",
+  "/docs/modal/api",
+  "/docs/modal/examples",
+  "/docs/modal/examples/drawer",
+  "/docs/modal/examples/lightbox",
+  "/docs/modal/releases",
+  "/docs/slider",
+  "/docs/slider/api",
+  "/docs/slider/setup",
+  "/docs/slider/examples",
+  "/docs/slider/examples/custom-scrollbar",
+  "/docs/slider/examples/dots",
+  "/docs/slider/examples/free-scrolling",
+  "/docs/slider/examples/lightbox",
+  "/docs/slider/examples/marquee",
+  "/docs/slider/examples/responsive",
+  "/docs/slider/examples/simple",
+  "/docs/slider/examples/snap",
+  "/docs/slider/examples/thumbnail",
+  "/docs/slider/releases",
+  "/docs/css-grid",
+  "/docs/css-grid/api",
+  "/docs/css-grid/releases",
+  "/docs/css-grid/setup",
+  "/docs/jumplist",
+  "/docs/jumplist/api",
+  "/docs/jumplist/setup",
+  "/docs/jumplist/releases",
+  "/docs/collapsibles/api",
+  "/docs/collapsibles",
+  "/docs/collapsibles/releases",
+  "/docs/collapsibles/setup",
+  "/docs/window-info",
+  "/docs/window-info/setup",
+  "/docs/window-info/api",
+  "/docs/window-info/releases",
+  "/docs/scroll-info",
+  "/docs/scroll-info/api",
+  "/docs/scroll-info/releases",
+  "/docs/scroll-info/setup",
+  "/docs/mouse-info/releases",
+  "/docs/mouse-info/api",
+  "/docs/mouse-info",
+  "/docs/mouse-info/setup",
+  "/releases",
 ];
 
-type Route = {
-  page: string
-}
+
+// const blacklistedPages = [
+//   '/sitemap.xml',
+//   '/.DS_Store',
+//   '/404',
+//   '/500',
+//   '/_error',
+//   '/_app',
+//   '/_document',
+//   '/styleguide',
+// ];
+
+// type Route = {
+//   page: string
+// }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
   const { res } = context;
 
-  const staticPaths: string[] = [];
+  const staticPaths: string[] = pages;
 
-  if (process.env.NODE_ENV === 'production') {
-    try {
-      const file = await fs.readFile(`.next/routes-manifest.json`);
-      const routesManifest = await JSON.parse(file as unknown as string);
-      if (routesManifest.staticRoutes) {
-        routesManifest.staticRoutes.forEach((route: Route) => {
-          const { page } = route;
-          if (page && !blacklistedPages.includes(page)) {
-            staticPaths.push(page);
-          }
-        });
-      }
-    } catch (err) {
-      console.error(err);
-    }
-  }
+  // NOTE: this doesn't work on Vercel, but does locally in production
+  // if (process.env.NODE_ENV === 'production') {
+  //   try {
+  //     const file = await fs.readFile(`.next/routes-manifest.json`);
+  //     const routesManifest = await JSON.parse(file as unknown as string);
+  //     if (routesManifest.staticRoutes) {
+  //       routesManifest.staticRoutes.forEach((route: Route) => {
+  //         const { page } = route;
+  //         if (page && !blacklistedPages.includes(page)) {
+  //           staticPaths.push(page);
+  //         }
+  //       });
+  //     }
+  //   } catch (err) {
+  //     console.error(err);
+  //   }
+  // }
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
     <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
       <url>
-        <loc>${process.env.NEXT_PUBLIC_APP_URL}</loc>
+        <loc>${process.env.NEXT_PUBLIC_APP_URL}/</loc>
         <lastmod>${new Date().toISOString()}</lastmod>
         <changefreq>weekly</changefreq>
         <priority>1.0</priority>
